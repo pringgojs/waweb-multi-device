1dc6dffedaf9e70e75414fe4c46d9a5d810cc995
Pringgo Juni S <odyinggo@gmail.com>
2022-04-12 15:02:22 +0700

    fix bug

--- middleware/error.js
+++ middleware/error.js
@@ -1,36 +1,36 @@
-const ErrorResponse = require('../utils/ErrorResponse')
+const ErrorResponse = require("../utils/errorResponse");
 
 const errorHandler = (err, req, res, next) => {
-    let error = {
-        ...err
-    }
+  let error = {
+    ...err,
+  };
 
-    error.message = err.message
-    // console.log(err.stack.red)
-    console.log(err.name)
+  error.message = err.message;
+  // console.log(err.stack.red)
+  console.log(err.name);
 
-    // Mongoose bad objectID
-    if (err.name === 'CastError') {
-        const message = `Resource not found with ID ${err.value}`
-        error = new ErrorResponse(message, 404)
-    }
+  // Mongoose bad objectID
+  if (err.name === "CastError") {
+    const message = `Resource not found with ID ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
 
-    // Mongoose duplicate key
-    if (err.code === 11000) {
-        const message = `Duplicate fields value entered`
-        error = new ErrorResponse(message, 400)
-    }
+  // Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = `Duplicate fields value entered`;
+    error = new ErrorResponse(message, 400);
+  }
 
-    // Mongoose validation error
-    if (err.name === 'ValidationError') {
-        const message = Object.values(err.errors).map(val => val.message)
-        error = new ErrorResponse(message, 400)
-    }
+  // Mongoose validation error
+  if (err.name === "ValidationError") {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
 
-    res.status(error.statusCode || 500).json({
-        success: false,
-        error: error.message || 'Server Error'
-    })
-}
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || "Server Error",
+  });
+};
 
-module.exports = errorHandler
+module.exports = errorHandler;
