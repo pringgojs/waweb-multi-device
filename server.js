c5c9dad87e251c63432b4f1d53bd2a4e76b271a4
Pringgo Juni S <odyinggo@gmail.com>
2022-04-12 15:25:21 +0700

    fix bug

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
 const client = require("./utils/client");
 
 // Load env vars
